f2c4f3120c81b4b962cc4ce0bd55dccd33e3ce80
ardalanfp <aforoughipour@gmail.com>
2019-02-03 12:21:25 +0330

    setting ip in application properties file

--- src/main/java/com/microservice/courseTaking/courseManager/controller/CourseController.java
+++ src/main/java/com/microservice/courseTaking/courseManager/controller/CourseController.java
@@ -3,25 +3,33 @@ package com.microservice.courseTaking.courseManager.controller;
 import com.microservice.courseTaking.courseManager.model.Course;
 import com.microservice.courseTaking.courseManager.repository.CourseRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
+
+
+//@ResponseStatus(value = HttpStatus.NOT_FOUND)
+//class ResourceNotFoundException extends RuntimeException {
+//
+//}
 
 
 @RestController
 public class CourseController {
 
+
     @Autowired
     CourseRepository repository;
 
+    @Value("${AuthServiceIp}")
+    private String authServiceIp;
 
     private String getRole(String jwttoken){
-        final String uri = "http://172.21.50.142:8080/getRole";
+        final String uri = "http://"+authServiceIp+":8080/getRole";
         System.out.println(jwttoken);
         HttpHeaders headers = new HttpHeaders();
         headers.setBearerAuth(jwttoken);
@@ -68,6 +76,16 @@ public class CourseController {
 
     }
 
+    @RequestMapping(value = "/exists")
+    public String courseExists(Integer courseid){
+        if(repository.findByid(courseid) != null){
+            return "ok";
+        }
+        else{
+            //throw new ResourceNotFoundException();
+            return "not found";
+        }
+    }
 
 
 }
--- src/main/java/com/microservice/courseTaking/courseManager/repository/CourseRepository.java
+++ src/main/java/com/microservice/courseTaking/courseManager/repository/CourseRepository.java
@@ -4,7 +4,9 @@ import com.microservice.courseTaking.courseManager.model.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CourseRepository extends JpaRepository<Course,Integer> {
-
+    Course findByid(Integer integer);
 }
